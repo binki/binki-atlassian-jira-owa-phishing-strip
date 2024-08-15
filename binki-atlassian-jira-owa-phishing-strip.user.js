74544ff06361ab1afa9b8bba5bfd4136c4077074
Nathan Phillip Brink <ohnobinki@ohnopublishing.net>
2024-08-15 12:14:01 -0400

    Add support for the text version of the prefix.
    Some emails came in which ended up with the text version of the prefix even though the email seemed to be HTML and to have a table at the beginning itself. Odd. But now this supports more formats.

--- binki-atlassian-jira-owa-phishing-strip.user.js
+++ binki-atlassian-jira-owa-phishing-strip.user.js
@@ -83,31 +83,89 @@ async function editAtlassianDocumentAsync(document) {
   try {
   	if (document.type !== 'doc') throw new Error(`Top-level document element must be “doc”. Got “${document.type}”`);
     if (!document.content.length) return false;
-    const table = document.content[0];
-    // This might be incapable of handling the plaintext version?
-    if (table.type !== 'table') return false;
-    if (table.content.length !== 1) return false;
-    const tableRow = table.content[0];
-    if (tableRow.type !== 'tableRow') return false;
-    for (const tableCell of tableRow.content) {
-      if (tableCell.type !== 'tableCell') continue;
-      const textContent = atlassianDocumentTextContent(tableCell);
-      if (/^\s*You don't often get email from [^\s]+\.\s+Learn why this is important\s*$/v.test(textContent)) {
-        document.content.splice(0, 1);
-        // A very specific test for extra empty paragraphs that might be left over at the beginning. Don’t
-        // simply test the text content of it because it might be something important but empty like an image
-        // without alt text or a card.
-        while (document.content.length && document.content[0].type === 'paragraph' && document.content[0].content.length === 1 && document.content[0].content[0].type === 'text' && /^\s*$/v.test(atlassianDocumentTextContent(document.content[0]))) {
-					document.content.splice(0, 1);
-        }
-        console.log(textContent, document);
-        return true;
-      }
-    }
+    
+	  // There are different document variants. Table version:
+    if (await editTableAtlassianDocumentAsync(document)) return true;
+
+    // Text version
+    if (await editTextAtlassianDocumentAsync(document)) return true;
+    
     return false;
   } catch (ex) {
     console.log('error editing document', document, ex);
     throw ex;
+  }
+}
+
+async function editTableAtlassianDocumentAsync(document) {
+  const table = document.content[0];
+  if (table.type !== 'table') return false;
+  if (table.content.length !== 1) return false;
+  const tableRow = table.content[0];
+  if (tableRow.type !== 'tableRow') return false;
+  for (const tableCell of tableRow.content) {
+    if (tableCell.type !== 'tableCell') continue;
+    const textContent = atlassianDocumentTextContent(tableCell);
+    if (/^\s*You don't often get email from [^\s]+\.\s+Learn why this is important\s*$/v.test(textContent)) {
+      document.content.splice(0, 1);
+      removeEmptyPrologue(document);
+      console.log(textContent, document);
+      return true;
+    }
+  }
+  return false;
+}
+
+async function editTextAtlassianDocumentAsync(document) {
+  // Format is like: {"type":"doc","version":1,"content":[{"type":"paragraph","content":[{"type":"text","text":" ||"},{"type":"text","text":"You don't often get email from noreply@example.com. ","marks":[{"type":"textColor","attrs":{"color":"#000000"}}]},{"type":"text","text":"Learn why this is important","marks":[{"type":"textColor","attrs":{"color":"#000000"}},{"type":"link","attrs":{"href":"https://aka.ms/LearnAboutSenderIdentification"}}]},{"type":"text","text":" ||"},{"type":"hardBreak"},{"type":"text","text":" "},{"type":"hardBreak"},{"type":"text","text":" "}]}]}
+  const paragraph = document.content[0];
+  if (paragraph.type !== 'paragraph') return false;
+  const normalizedText = atlassianDocumentTextContent(paragraph).replace(/\s+/gv, ' ');
+  if (/^ ?\|\| ?You don\S*t often get email from \S+ ?\. Learn why this is important ?\|\| ?$/v.test(normalizedText)) {
+    // Remove the paragraph
+    document.content.splice(0, 1);
+    removeEmptyPrologue(document);
+    return true;
+  }
+  return false;
+}
+
+function removeEmptyPrologue(document) {
+  // A very specific test for extra empty paragraphs and tables that might be left over at the beginning. Don’t
+  // simply test the text content of it because it might be something important but empty like an image
+  // without alt text or a card.
+  while (true) {
+    if (!document.content.length) break;
+    const first = document.content[0];
+    if (first.type === 'paragraph') {
+		  if (first.content.length > 1) break;
+      const text = first.content[0];
+      // Allow empty instead of requiring text.
+      if (text) {
+      	if (text.type !== 'text') break;
+      }
+    } else if (first.type === 'table') {
+      if (first.content.length > 1) break;
+      const row = first.content[0];
+      if (row) {
+        if (row.type !== 'tableRow') break;
+        if (row.content.length > 1) break;
+        const cell = row.content[0];
+        if (cell) {
+          if (cell.type !== 'tableCell') break;
+          // Now that we have a cell, verify that it has something like a paragraph or text in it.
+          if (cell.content.length > 1) break;
+          const cellFirst = cell.content[0];
+          if (cellFirst) {
+            if (cellFirst.type !== 'paragraph') break;
+          }
+        }
+      }
+    } else {
+      break;
+    }
+    if (!/^\s*$/v.test(atlassianDocumentTextContent(first))) break;
+    document.content.splice(0, 1);
   }
 }
 
